92ef45429df8dfaa811908afc4afc33ed5fc8ffb
Yi Fung <yifung2@blender01.cs.illinois.edu>
2021-05-09 15:37:29 -0500

    Updated code to compute and optimize loss

--- train_vdcnn.py
+++ train_vdcnn.py
@@ -4,14 +4,14 @@ from vdcnn_model import VDCNN
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-data_fpath = "data/yelp_review_polarity_csv/train.csv"
+data_fpath = "data/yelp_review_polarity_csv/"
 
-train_dataset = MyDataset(data_fpath)
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_dataset = MyDataset(data_fpath)
-val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
-test_dataset = MyDataset(data_fpath)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+train_dataset = MyDataset(data_fpath+"train.csv")
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_dataset = MyDataset(data_fpath+"val.csv")
+val_dataloader = DataLoader(val_dataset, batch_size=64)
+test_dataset = MyDataset(data_fpath+"test.csv")
+test_dataloader = DataLoader(test_dataset, batch_size=64)
 
 model = VDCNN()
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
@@ -21,12 +21,11 @@ for epoch in range(5):
     for idx, (x, y) in enumerate(train_dataloader):
         optimizer.zero_grad()
         outputs = model(x.long())
-        print(outputs.size())
-        loss = outputs.loss
+        loss = model.compute_loss(outputs, y)
         loss.backward()
         optimizer.step()
     model.eval()
     for idx, (x, y) in enumerate(val_dataloader):
         outputs = model(x)
 for idx, (x, y) in enumerate(test_dataloader):
-    outputs = model(x, y)
+    outputs = model(x)
--- vdcnn_model.py
+++ vdcnn_model.py
@@ -21,8 +21,7 @@ class VDCNN(nn.Module):
 
         layers = [nn.Conv1d(16, 64, kernel_size=3, padding=1)]
         for _ in range(4):
-            layers.append(ConvBlock(input_dim=64, n_filters=64, \
-                                     kernel_size=3, padding=1))
+            layers.append(ConvBlock(input_dim=64, n_filters=64, kernel_size=3, padding=1))
         layers.append(nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
 
         layers.append(ConvBlock(input_dim=64, n_filters=2*64, kernel_size=3, padding=1))
@@ -46,8 +45,13 @@ class VDCNN(nn.Module):
             nn.Linear(2048, 2048), nn.ReLU(), \
             nn.Linear(2048, n_classes)
             )
+            
+        self.ce_loss = nn.CrossEntropyLoss()
 
     def forward(self, x):
         x = self.embed(x).transpose(1,2)
         x = self.layers(x).view(x.size(0),-1)
         return self.cls(x)
+
+    def compute_loss(self, pred_y, true_y):
+        return self.ce_loss(pred_y, true_y)
