c62e71bb6738dea6cbb50809bfb395417408caec
Yi Fung <yifung2@blender06.cs.illinois.edu>
2021-05-10 08:10:39 -0500

    Added score metric

--- train_vdcnn.py
+++ train_vdcnn.py
@@ -13,19 +13,31 @@ val_dataloader = DataLoader(val_dataset, batch_size=64)
 test_dataset = MyDataset(data_fpath+"test.csv")
 test_dataloader = DataLoader(test_dataset, batch_size=64)
 
-model = VDCNN()
+num_classes = train_dataset.num_classes
+
+model = VDCNN(num_classes)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
 
 for epoch in range(5):
-    model.train()
+    """model.train()
     for idx, (x, y) in enumerate(train_dataloader):
         optimizer.zero_grad()
-        outputs = model(x.long())
-        loss = model.compute_loss(outputs, y)
+        logits = model(x.long())
+        loss = model.compute_loss(logits, y)
         loss.backward()
-        optimizer.step()
+        optimizer.step()"""
     model.eval()
+    err, tot_count = 0, 0
     for idx, (x, y) in enumerate(val_dataloader):
-        outputs = model(x)
+        logits = model(x.long())
+        pred_y = torch.argmax(logits, dim=1)
+        err += (pred_y != y).sum()
+        tot_count += y.size(0)
+    print("Val err: " + str(err/tot_count))
+err, tot_count = 0, 0
 for idx, (x, y) in enumerate(test_dataloader):
-    outputs = model(x)
+    logits = model(x.long())
+    pred_y = torch.argmax(logits, dim=1)
+    err += (pred_y != y).sum()
+    tot_count += y.size(0)
+print("Test err: " + str(err/tot_count))
